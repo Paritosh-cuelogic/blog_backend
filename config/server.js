151265ea605b02f3499aef7f2fdb1d058a0019d9
Paritosh <paritosh.mahale@gmail.com>
2021-06-24 11:38:47 +0530

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c9145e3e4e3de0480a847ad58c2731af'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b5564226-58e6-462c-b481-583c60155cc1'
+};
